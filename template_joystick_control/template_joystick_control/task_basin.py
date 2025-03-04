fix/joystick: transpose the rotation matrix
The rotation matrix needs to be transposed to rotate from NED
to body since the commands are interpreted in body frame.

template_joystick_control/template_joystick_control/task_basin.py
@@ -16,8 +16,8 @@ def rotate(heading) -> np.matrix:
         return np.sin(angle)
     return np.matrix(
         [
-            [c(heading), -s(heading), 0.0],
-            [s(heading), c(heading), 0.0],
+            [c(heading), s(heading), 0.0],
+            [-s(heading), c(heading), 0.0],
             [0.0, 0.0, 1.0],
         ], dtype=np.float64
     )
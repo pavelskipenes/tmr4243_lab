0ae4ece3f7e92d023d84efc9fec09814f5133f6e
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2025-04-10 16:09:28 +0200

    feat(controller): add PD_FF

--- template_controller/template_controller/PD_FF_controller.py
+++ template_controller/template_controller/PD_FF_controller.py
@@ -1,29 +1,56 @@
 
 import numpy as np
-from numpy._typing import NDArray
+from numpy.typing import NDArray
 import tmr4243_interfaces.msg
+from typing import Any
+
+
+def rotation_matrix(heading):
+    R = np.array([[np.cos(heading), -np.sin(heading), 0],
+                  [np.sin(heading),  np.cos(heading), 0],
+                  [0, 0, 1]])
+    return R
+
+
+def get_gains(M, D):
+    # TODO: refactor out
+    M = np.array([[16, 0, 0],
+                  [0, 24, 0.53],
+                  [0, 0.53, 2.8]])
+
+    D = np.array([[0.66, 0, 0],
+                  [0, 1.3, 2.8],
+                  [0, 0, 1.9]])
+    zeta = np.eye(3)
+    T_n = 10
+    w_n = (2*np.pi/T_n) * np.eye(3)
+
+    P_gain = (w_n**2) @ M
+    I_gain = np.eye(3) * 0
+    D_gain = 2 * zeta @ w_n @ M - D
+
+    return P_gain, D_gain, I_gain
 
 
 def PD_FF_controller(
-        observer: tmr4243_interfaces.msg.Observer,
-        reference: tmr4243_interfaces.msg.Reference,
-        P_gain: np.float64,
-        D_gain: np.float64) -> NDArray[np.float64]:
+    observer: tmr4243_interfaces.msg.Observer,
+    reference: tmr4243_interfaces.msg.Reference,
+    P_gain: NDArray[np.float64],
+    I_gain: Any,
+    D_gain: NDArray[np.float64],
+) -> np.ndarray:
+    # try to keep the same interface between controllers
+    _ = I_gain
 
-    tau = np.zeros((3, 1), dtype=np.float64)
+    eta_hat = np.array(observer.eta)
+    nu_hat = np.array(observer.nu)
+    eta_d = np.array(reference.eta_d)
 
-    # Getting the states from the observer
-    eta_hat = observer.eta
-    nu_hat = observer.nu
-    bias_hat = observer.bias
+    heading = eta_hat[2]
+    R = rotation_matrix(heading)
 
-    # Getting the states from the refernce
-    eta_d = reference.eta_d
-    eta_ds = reference.eta_ds
-    eta_ds2 = reference.eta_ds2
-    w = reference.w
-    v_s = reference.v_s
-    v_ss = reference.v_ss
+    error_position_body = R.T @ (eta_hat - eta_d)
+
+    tau = -P_gain @ error_position_body - D_gain @ nu_hat
 
     return tau
-
